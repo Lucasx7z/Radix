03abc622906ec9106d4f550176d1d050bf7995f4
Lucasx7z <46058972+Lucasx7z@users.noreply.github.com>
2019-11-25 22:48:58 -0300

    Add files via upload

--- ExercícioEDB.c
+++ ExercícioEDB.c
@@ -1,10 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<windows.h>
+#define teste1 22
+
 #include "ExercícioEDB.h"
 
 int main(){
-    int *v = NULL;
-    v = criarVetor(v,1000);
-    Radix(v,20);
+
+    int *vetR = NULL, *vetQ = NULL, i;
+    float tempo_total, sumTimeR = 0, sumTimeQ = 0;
+
+    LARGE_INTEGER inicio, fim, freq;
+
+    vetQ = criarVetor(vetQ,teste1);
+	vetR = criarVetor(vetR,teste1);
+
+	QueryPerformanceFrequency(&freq);
+
+    QueryPerformanceCounter(&inicio);
+    Radix(vetR,teste1);
+    QueryPerformanceCounter(&fim);
+    tempo_total = (float)(fim.QuadPart - inicio.QuadPart)/freq.QuadPart;
+
+	printf("Tempo Medio de execucao (Radix): %f",tempo_total);
 }
